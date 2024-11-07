b9a65b254ffcac523cc045722d77b7e43d378211
Maria Camille Rivera <130654405+mrcmllrvr@users.noreply.github.com>
2024-11-07 12:17:10 +0800

    Update ChatbotUI.py

--- ChatbotUI.py
+++ ChatbotUI.py
@@ -105,7 +105,8 @@ with st.sidebar:
 import chromadb
 from chromadb.utils import embedding_functions
 
-CHROMA_DATA_PATH = 'chromadb_major_travel/'
+# CHROMA_DATA_PATH = 'chromadb_major_travel/'
+CHROMA_DATA_PATH = 'major_travel_uat_phase2/'
 HYPO_COLLECTION_NAME = "hypothetical_embeddings"
 DOCS_COLLECTION_NAME = "document_embeddings"
 
